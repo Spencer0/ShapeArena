3d8a30fb0cb62fff8ed8a030ddb19d57272d879d
Spencer Strickland <spencerstrickland1@hotmail.com>
2020-06-02 13:26:03 -0700

    Null checking a different way for type saftey (pls ts soon)

--- src/shape-scooter.js
+++ src/shape-scooter.js
@@ -123,7 +123,7 @@ function ShapeScooter(){
                             if(this.collides(this.state.player[playerIdInternal], bullet)){
                                 this.respawnPlayer(playerIdInternal);
                                 this.levelUp(playerId, 2);
-                                if(!io) { return }
+                                if(typeof io === 'undefined') { return }
                                 io.sockets.emit('state', this.state);
                             }
                         }
